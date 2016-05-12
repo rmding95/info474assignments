2049f4dcef45aa6c5342ddcf5d15d99c6e7c2fec
Richard Ding <rmding95@uw.edu>
2016-05-11 18:50:52 -0700

    finished

--- visual-soft-design/my.js
+++ visual-soft-design/my.js
@@ -2,7 +2,7 @@
 
 d3.csv('data.csv', function(error, data) {
 	console.log(data);
-	var myChart = PieChart().category('age').category_values('population').width(1000).height(1000).colorRange(['red', 'green', 'blue']);
+	var myChart = PieChart().category('name').category_values('age').width(1000).height(1000).colorRange(['red', 'green', 'blue']);
 	var chartWrapper = d3.select("#vis").datum(data).call(myChart);
 });
 
